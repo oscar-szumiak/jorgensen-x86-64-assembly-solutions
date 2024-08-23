9ea06f2b2cc2b4fe44dde2ecb509c0c842ce9f98
Oscar Szumiak <oscarszumiak@gmail.com>
2024-08-23 02:07:38 +0200

    Addition of 7.9 prj-8

---
+++ 7.9.2-instructions/prj-8/in.gdb
@@ -0,0 +1,20 @@
+#------------------------------------
+#  Debugger Input Script
+#------------------------------------
+echo \n\n
+break last
+run
+set pagination off
+set logging file out.txt
+set logging overwrite
+set logging on
+set prompt
+echo sum: \n
+x/dw &sum
+x/xw &sum
+echo squareOfSum: \n
+x/dg &squareOfSum
+x/xg &squareOfSum
+echo \n\n
+set logging off
+quit
---
+++ 7.9.2-instructions/prj-8/square-of-sum.asm
@@ -0,0 +1,57 @@
+; Create a program to compute the square of the sum from 1 to n. Specifically,
+; compute the sum of integers from 1 to n and then square the value. Use the
+; debugger to execute the program and display the final results.
+; Create a debugger input file to show the results in both decimal
+; and hexadecimal.
+
+;  Data declarations
+
+section .data
+
+; -----
+;  Define constants
+
+SUCCESS         equ     0       ; Successful operation
+SYS_exit        equ     60      ; call code for terminate
+
+;  Define Data.
+
+n               dd      10
+sum             dd      0
+squareOfSum     dq      0
+
+; *******************************************************
+
+section .text
+global _start
+_start:
+
+; -----
+;  Compute sum of squares from 1 to n (inclusive).
+;  Approach:
+;   for (i=1; i<=n; i++)
+;       sum += i;
+
+    mov rbx, 1                      ; i
+    mov ecx, dword [n]
+sumLoop:
+    add dword [sum], ebx
+    inc rbx
+    loop sumLoop
+
+; -----
+; squareOfSum = sum * sum
+
+    mov eax, dword [sum]
+    mul eax
+    mov dword [squareOfSum], eax
+    mov dword [squareOfSum+4], edx
+
+; -----
+;  Done, terminate program.
+
+last:
+    mov rax, SYS_exit ; call code for exit
+    mov rdi, SUCCESS ; exit with success
+    syscall
+
