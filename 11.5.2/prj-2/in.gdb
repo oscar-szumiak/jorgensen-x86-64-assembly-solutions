a49d697f19bcd190beafa80cd6acd26c7f7a5430
Oscar Szumiak <oscarszumiak@gmail.com>
2024-09-11 22:17:39 +0200

    Addition of 11.5.2 prj-2

---
+++ 11.5.2/prj-2/in.gdb
@@ -0,0 +1,26 @@
+#------------------------------------
+#  Debugger Input Script
+#------------------------------------
+echo \n\n
+break last
+run
+set pagination off
+set logging file out.txt
+set logging overwrite
+set logging on
+set prompt
+echo ave1: \n
+x/dw &ave1
+echo min1: \n
+x/dw &min1
+echo max1: \n
+x/dw &max1
+echo ave2: \n
+x/dw &ave2
+echo min2: \n
+x/dw &min2
+echo max2: \n
+x/dw &max2
+echo \n\n
+set logging off
+quit
---
+++ 11.5.2/prj-2/list-avg-min-max-macro.asm
@@ -0,0 +1,96 @@
+; Update the program from the previous question to include the minimum
+; and maximum values.
+; Use the debugger to execute the program and display the final results.
+; Create a debugger input file to show the results.
+
+; **************************************************
+; Define the macro called with five arguments:
+; aver <lst>, <len>, <ave>, <min>, <max>
+
+%macro aver 5
+    mov     r12, 0
+    lea     rbx, [%1]
+
+    mov     edx, dword [rbx]
+    mov     dword [%4], edx
+    mov     edx, dword [rbx]
+    mov     dword [%5], edx
+    
+    mov     eax, 0
+    mov     ecx, dword [%2]             ; length
+
+%%sumLoop:
+    mov     edx, dword [rbx+r12*4]      ; get list[n]
+
+    cmp     edx, dword [%4]
+    jg      %%noNewMin
+
+    mov     dword [%4], edx
+    jmp     %%noNewMax
+
+%%noNewMin:
+
+    cmp     edx, dword [%5]
+    jl      %%noNewMax
+
+    mov     dword [%5], edx
+
+%%noNewMax:
+
+    add     eax, edx                    ; add list[n]
+    inc     r12
+    loop    %%sumLoop
+    
+    cdq
+    idiv    dword [%2]
+    mov     dword [%3], edx
+
+%endmacro
+
+; **************************************************
+; Data declarations
+
+section     .data
+
+; -----
+; Define constants
+
+EXIT_SUCCESS    equ 0          ; success code
+SYS_exit        equ 60         ; code for terminate
+
+; Define Data.
+
+section     .data
+
+list1       dd      4, 5, 2, -3, 1
+len1        dd      5
+ave1        dd      0
+min1        dd      0
+max1        dd      0
+
+list2       dd      2, 6, 3, -2, 1, 8, 19
+len2        dd      7
+ave2        dd      0
+min2        dd      0
+max2        dd      0
+
+; **************************************************
+
+section     .text
+global _start
+_start:
+
+; -----
+; Use the macro in the program
+
+aver    list1, len1, ave1, min1, max1       ; 1st, data set 1
+aver    list2, len2, ave2, min2, max2       ; 2nd, data set 2
+
+; -----
+; Done, terminate program.
+
+last:
+    mov     rax, SYS_exit
+    mov     rdi, EXIT_SUCCESS
+    syscall
+
