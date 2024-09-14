d5808c4578958d541aecb9f9dc84bfe10cd5daad
Oscar Szumiak <oscarszumiak@gmail.com>
2024-09-14 04:46:38 +0200

    Addition of 11.5.2 prj-4

---
+++ 11.5.2/prj-4/in.gdb
@@ -0,0 +1,26 @@
+#------------------------------------
+#  Debugger Input Script
+#------------------------------------
+echo \n\n
+break last
+run
+set pagination off
+set logging file out.txt
+set logging overwrite
+set logging on
+set prompt
+echo intNum1: \n
+x/uw &intNum1
+echo strNum1: \n
+x/s &strNum1
+echo intNum2: \n
+x/uw &intNum2
+echo strNum2: \n
+x/s &strNum2
+echo intNum3: \n
+x/uw &intNum3
+echo strNum3: \n
+x/s &strNum3
+echo \n\n
+set logging off
+quit
---
+++ 11.5.2/prj-4/uint-to-string-macro.asm
@@ -0,0 +1,88 @@
+; Create a macro from the integer to ASCII conversion example
+; from the previous chapter.
+; Invoke the macro at least three times of three different data sets.
+; Use the debugger to execute the program and display the final results.
+; Create a debugger input file to show the results.
+
+; *********************************************************
+; Data declarations
+
+; Convert an integer to an ASCII string
+; uint-to-string <int> <string>
+%macro uintToString 2
+
+; -----
+; Part A - Successive division
+
+    mov     eax, dword [%1]                 ; get integer
+
+    mov     rcx, 0                          ; digitCount = 0
+    mov     ebx, 10                         ; set for dividing by 10
+%%divideLoop:
+    mov     edx, 0
+    div     ebx                             ; divide number by 10
+
+    push    rdx                             ; push remainder
+    inc     rcx                             ; increment digitCount
+
+    cmp     eax, 0                          ; if (result > 0)
+    jne     %%divideLoop                    ; goto divideLoop
+
+; -----
+; Part B - Convert remainders and store
+
+    lea     rbx, [%2]                       ; get addr of string
+    mov     rdi, 0                          ; idx = 0
+%%popLoop:
+    pop     rax                             ; pop intDigit
+    add     al, "0"                         ; char = int + "0"
+    mov     byte [rbx+rdi], al              ; string[idx] = char
+    inc     rdi                             ; increment idx
+    loop    %%popLoop                       ; if (digitCount > 0)
+                                            ; goto popLoop
+    mov     byte [rbx+rdi], NULL            ; string[idx] = NULL
+
+%endmacro
+
+section .data
+
+; -----
+; Define constants
+
+NULL                equ     0
+EXIT_SUCCESS        equ     0           ; successful operation
+SYS_exit            equ     60          ; code for terminate
+
+; -----
+; Define Data.
+
+intNum1          dd      1786
+intNum2          dd      348
+intNum3          dd      86
+
+section .bss
+
+strNum1          resb    10
+strNum2          resb    10
+strNum3          resb    10
+
+; *********************************************************
+
+section .text
+global _start
+_start:
+
+; Macro calls
+
+    uintToString      intNum1, strNum1
+    uintToString      intNum2, strNum2
+    uintToString      intNum3, strNum3
+
+; -----
+; Done, terminate program
+
+last:
+    mov rax, SYS_exit                       ; call code for exit
+    mov rdi, EXIT_SUCCESS                   ; exit with success
+    syscall
+
