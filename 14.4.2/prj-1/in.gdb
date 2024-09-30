1e51f27a3ed4c5ac76b45cdc23338ccb6dd1253c
Oscar Szumiak <oscarszumiak@gmail.com>
2024-09-30 18:57:31 +0200

    Addition of 14.4.2 prj-1

---
+++ 14.4.2/prj-1/in.gdb
@@ -0,0 +1,26 @@
+#------------------------------------
+#  Debugger Input Script
+#------------------------------------
+echo \n\n
+break last
+run
+set pagination off
+set logging file out.txt
+set logging overwrite
+set logging on
+set prompt
+echo list1: \n
+x/5dw &list1
+echo sum1: \n
+x/dw &sum1
+echo ave1: \n
+x/dw &ave1
+echo list2: \n
+x/7dw &list2
+echo sum2: \n
+x/dw &sum2
+echo ave2: \n
+x/dw &ave2
+echo \n\n
+set logging off
+quit
---
+++ 14.4.2/prj-1/main.asm
@@ -0,0 +1,53 @@
+; Implement the assembly language example program to find the sum and average
+; for a list of signed integers. Ensure that the main and function
+; are in different source files. Use the debugger to execute the program
+; and display the final results.
+
+section     .data
+
+EXIT_SUCCESS    equ 0
+SYS_exit        equ 60 
+
+list1       dd      4, 5, 2, -3, 1
+len1        dd      5
+sum1        dd      0
+ave1        dd      0
+
+list2       dd      2, 6, 3, -2, 1, 8, 19
+len2        dd      7
+sum2        dd      0
+ave2        dd      0
+
+; **************************************************
+
+extern stats
+
+section     .text
+
+global _start
+_start:
+
+; Call function stats for list1
+
+    mov     rdi, list1
+    mov     esi, dword [len1]
+    mov     rdx, sum1
+    mov     rcx, ave1
+    call    stats
+
+; Call function stats for list1
+
+    mov     rdi, list2
+    mov     esi, dword [len2]
+    mov     rdx, sum2
+    mov     rcx, ave2
+    call    stats
+
+; -----
+; Done, terminate program.
+
+last:
+    mov     rax, SYS_exit
+    mov     rdi, EXIT_SUCCESS
+    syscall
+
