5a8fd78032bede702e863cacca01d27af24c35c8
Oscar Szumiak <oscarszumiak@gmail.com>
2024-11-07 22:59:07 +0100

    Major fix of 20.5.2 prj-1

--- 20.5.2/prj-1/intToHex.asm
+++ 20.5.2/prj-1/intToHex.asm
@@ -46,6 +46,7 @@ checkBuffer:
     mov     rax, rsi
     mov     r8, 2
     mul     r8                      ; Calculate number of ascii hex bytes
+    mov     r11, rax
     cmp     rcx, rax                ; and check against hexBuffer size
     jae     convert
 
@@ -56,24 +57,29 @@ convert:
     mov     rcx, 0                  ; Index of input data
     mov     rax, 0                  ; Temporary input byte variable
     mov     r10, 0                  ; Temporary output byte variable
-    mov     r11, 0                  ; Index of hexBuffer
 hexLoop:
     mov     al, byte [r12+rcx]
     and     rax, 0x000000000000000F         ; Get lower 4 bits
     mov     r10b, byte [hexValues+rax]
-    mov     byte [r14+r11], r10b
-    inc     r11
+    push    r10
 
     mov     al, byte [r12+rcx]
     shr     al, 4                           ; Get upper 4 bits
     mov     r10b, byte [hexValues+rax]
-    mov     byte [r14+r11], r10b
-    inc     r11
+    push    r10
     
     inc     rcx
 
     cmp     rcx, r13
     jb      hexLoop
+
+    mov     rsi, 0
+    mov     rcx, r11
+popLoop:
+    pop     r10
+    mov     byte [r14+rsi], r10b
+    inc     rsi
+    loop    popLoop
 
     mov     rax, r11
 
